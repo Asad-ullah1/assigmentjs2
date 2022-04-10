c9a534afe8b11add5654b606b0a19078402b8cdf
Asad <52581107+Asad-ullah1@users.noreply.github.com>
2022-04-10 12:55:15 +0500

    firstcommit
    jsassigmnt2

---
+++ app.js
@@ -0,0 +1,306 @@
+
+ 
+            // Question 1
+
+// var asad = [];
+// console.log(name);
+
+                  // Question 2
+
+// var  khan = new Array();
+// console.log(khan);
+
+                  // Question 3
+
+// var arry ;
+// user = ["asim", "kashif"];
+// console.log(arry)
+
+                // Question 4
+
+// var age ;
+// userId = [19 , 20 , 40 , 51];
+// console.log(age);
+
+            
+             // Question 5
+
+// var user ;
+// used = [true , false];
+// console.log(user);
+
+
+                 // Question 6
+
+// var app ;
+// userI = ["Hello" ,23,true ];
+// console.log(app);
+
+
+            // Question7
+// var matric ;
+// matric = ["SSC","HSC","BSC","PHD","M,phil","BCOM","MS"];
+// document.write(matric);
+
+// var arr = [
+//     ["1) HSC","2) SSC"],
+//     ["3) M.PHIL","4) MA"],
+//     ["5) PHD","6) BA"]
+// ]
+
+// for(var row = 0; row < arr.length; row++){
+//     for(var col = 0; col < arr[row].length;col++){
+//         console.log(arr[row][col])
+//     }
+// }
+
+                 // Question 8
+
+
+// var Name = ["arsalan","shahid","asad"];
+// var Mark = [400,450,240];
+
+// var per = (Mark[0]/500 *100);
+// var per1 = (Mark[1]/500 * 100);
+// var per2 = (Mark[2]/500 *100);
+
+// document.write("Score Of "+Name[0]+" is "+Mark[0]+" and per is = " + per +"% </br>")
+// document.write("Score Of "+Name[1]+" is "+Mark[1]+" and per is = " + per1 +"%</br>")
+// document.write("Score Of "+Name[2]+" is "+Mark[2]+" and per is = " + per2 +"%</br>")
+
+                  // Question 1
+
+// var cityName = [" karachi "," quitta "," gilgit "," islamabad "];
+// console.log(cityName); 
+
+// //  A
+// cityName.unshift("multan");
+// console.log(cityName)
+
+// //  B
+// cityName.push("hunza");
+// console.log(cityName)
+
+//     // C
+// cityName.unshift("multan","hunza");
+// console.log(cityName)
+
+
+// //  D
+// cityName.shift();
+// console.log(cityName)
+
+// /  E
+// cityName.pop();
+// console.log(cityName)
+
+
+//// F
+// var arry = prompt("Enter index number");
+// var array2 = prompt("Enter Colour Name");
+// cityName.splice(num,0,coname);
+// console.log(cityName);
+
+
+// ////  G 
+// var Index = prompt("Enter index number");
+// var toindex = prompt("Enter number delete");
+// cityName.splice(Index,toindex,0);
+// console.log(cityName)
+
+            // Question  10
+
+// var a = [420,228,180,460,422,245];
+// console.log(a);
+
+// a.sort();
+// console.log(a)
+
+            // Question 11
+// document.write("<h1>Cites </h1></br>")
+// var cities = [" Karachi "," islamabad "," Sindh "," quetta "];
+// document.write(cities)
+
+// document.write("<h1>down cities:</h1></br>")
+// var slCity = cities.slice(2,4);
+// document.write(slCity)
+
+            /// Question 12
+
+// var arr = [" This " ,  " is ",   " my ", " cat "]
+// console.log("ARRAY  " + arr.join());
+//  console.log("String " + arr.join(''));
+
+            // Question13
+//    var num = prompt(" number of index")
+//  var array = []
+//   for(var i = 0; i < num; i++){
+//       array.push(prompt("enter order"))
+//    }
+//    for(var i = 0; i < n; i++){
+// document.write('Out:')    
+// document.write(array[i]+"</br>")
+
+// else{
+    //     document.write("Invalid order")
+    // }
+    // }
+//    }
+
+            // Question 14
+
+//   var num = prompt(" number of index")
+//  var age = []
+//   for(var i = 0; i < num; i++){
+//     age.push(prompt("enter orderd"))
+//  }
+// for(var i = age.length-1 ; i >= 0; i--){
+//     document.write('Out:')
+//      document.write(age[i]+"</br>")
+// else{
+//     document.write("Invalid order")
+// }
+// }
+
+            // Question 15
+// var age = ["Qmobile","Motrola","Samsung","Nokia",];
+
+// for(var i = 0; i <= age.length;i++){
+//     console.log(age[i])
+// };
+// else{
+//     console.log("Invaid Phone")
+// }
+
+
+
+
+
+
+                     //................. //
+
+
+
+
+
+//                       chapter 2
+
+
+
+
+// question1
+
+// var multi_Array = [
+//     ["asd",12 ,],
+//  ["kashif",32 ,1998 ,"karachi",]
+// ];
+
+// question2
+
+// var cls = [
+//         [0,1,2,3],
+//         [2,9,8,6],
+//         [23,4,5,6]
+//     ];
+
+// document.write(cls[0]+'<br>');
+// document.write(cls[1]+'<br>');
+// document.write(cls[2]+'<br>');
+   
+// question3
+
+// for (var i = 1;i<=10;i++){
+//     console.log(i + "</br>")
+// }
+
+// question
+4
+// var table_num = +prompt("Enter Table number");
+// var table_length = +prompt("Enter Table length");
+// for(var i = 1; i<=table_length;i++){
+//     document.write(table_num +" x "+i+" = "+ table_num*i+"<br>")
+// }
+
+// // question 5
+// var city_name = ["islamabad","quetta","lahore","karchi","uk","isl"];
+// for(var e = 0;e < city_name.length;e++){
+//     document.write(city_name[e]+'<br>')
+// }
+
+// for(var e = 0;e < city_name.length;e++){
+//     document.write("Element at index "+ e + " is " + city_name[e]+'<br>');
+// }
+
+// // question 6
+
+// document.write('<h2>Counting<h2>')
+// for(var f = 1; f <= 15;f++){
+//     document.write(f + ',')
+// } 
+// document.write('<h2>Reverse count<h2>')
+// for(var f = 10; f >= 1;f--){
+//     document.write(f + ',')
+// } 
+// document.write('<h2>Even<h2>')
+// for(var f = 0; f <=20;f+=2){
+//     document.write(f + ',')
+// } 
+// document.write('<h2>Odd<h2>')
+// for(var f = 1; f <= 19;f+=2){
+//     document.write(f + ',')
+// } 
+// document.write('<h2>series<h2>')
+// for(var f = 2; f <=20 ;f+=2){
+//     document.write(f + "k"+',')
+// } 
+
+// // question7
+
+// var A = ["garbs","manfo","slice","rse","apple"];
+// var user = prompt("Enter Furite Name");
+// var maych = false;
+
+// for (var g = 0 ; g < A.length;g++){
+//     if(user===A[g]){
+//         document.write(user + " is Avaliable at index "+ g +" in our mart ")
+//   maych=true;
+//   break;
+//   }
+// }
+
+// if (maych === false){
+//     document.write( "Sorry "+user +" not Avaliable ")
+
+// }
+
+// // question8
+// for (var i=0; i<=largest;i++){
+    //     if (array[i]>largest) {
+    //         var largest=array[i];
+    //     }
+    // }
+    // document.write("arrays item : " + array + "</br>" );
+    // document.write("the largest number is " + largest);
+    
+
+    
+    //  Question 9 
+    
+    // var age = [24, 53, 78, 91, 12]
+    // var ary= 100000000;
+    
+    // for (i=0; i<=ary;i++){
+    //     if (age[i]<ary) {
+    //         var ary=age[i];
+    //     }
+    // }
+    // document.write("age item  " + age + "</br>");
+    // document.write("the Smallest number is " + ary);
+    
+    
+    
+    //  Question10
+    
+    // for(var i=5 ; i<=100 ; i+=5){
+    //         document.write(i +"  ," )
+    //     }
